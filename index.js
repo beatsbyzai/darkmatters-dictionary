45ccca3a58e966c7aa47a2454300dddc69a18f62
Zainab Aliyu <zoaliyu@gmail.com>
2020-09-01 11:00:15 -0400

    make draggable

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 $( document ).ready(function() {
 
+  $("#intro-link").click(function(){
+    $("#introduction").removeClass("introduction-z").addClass("current-z");
+    $("#contributors").addClass("contributors-z").removeClass("current-z");
+    $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
+  });
 
-});
-
-$(function() {
-    $( "#introduction" ).draggable();
-  } );
-
-$("#intro-link").click(function(){
-  $("#introduction").addClass("current-z");
-});
-
-$("#contrib-link").click(function(){
-  $("#contributiors").addClass("current-z").removeClass("contributiors-z").;
-});
+  $("#contrib-link").click(function(){
+    $("#contributors").removeClass("contributors-z").addClass("current-z");
+    $("#introduction").addClass("introduction-z").removeClass("current-z");
+    $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
+  });
 
 
-$("#ack-link").click(function(){
-  $("#acknowledgements").addClass("current-z");
+  $("#ack-link").click(function(){
+    $("#acknowledgements").removeClass("acknowledgements-z").addClass("current-z");
+    $("#contributors").addClass("contributors-z").removeClass("current-z");
+    $("#introduction").addClass("introduction-z").removeClass("current-z");
+  });
+
+  $(function() {
+      $( ".page" ).draggable();
+    });
+
 });
