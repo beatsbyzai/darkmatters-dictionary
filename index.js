a83e1b29d9bac10ff830f18a4a4b93b66ce9e979
Zainab Aliyu <zoaliyu@gmail.com>
2020-09-01 18:56:52 -0400

    magin n pages

--- index.js
+++ index.js
@@ -14,21 +14,25 @@ $( document ).ready(function() {
     $(".link-stack").removeClass("hide");
   });
 
+  $("#link-hide").click(function(){
+    $(".page").toggleClass("hidepages");
+  });
+
   $("#intro-link").click(function(){
-    $("#introduction").removeClass("introduction-z").addClass("current-z");
+    $("#introduction").removeClass("introduction-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
     $("#contributors").addClass("contributors-z").removeClass("current-z");
     $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
   });
 
   $("#contrib-link").click(function(){
-    $("#contributors").removeClass("contributors-z").addClass("current-z");
+    $("#contributors").removeClass("contributors-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
     $("#introduction").addClass("introduction-z").removeClass("current-z");
     $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
   });
 
 
   $("#ack-link").click(function(){
-    $("#acknowledgements").removeClass("acknowledgements-z").addClass("current-z");
+    $("#acknowledgements").removeClass("acknowledgements-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
     $("#contributors").addClass("contributors-z").removeClass("current-z");
     $("#introduction").addClass("introduction-z").removeClass("current-z");
   });
@@ -36,19 +40,19 @@ $( document ).ready(function() {
   $("#link-poetic-computation").click(function(){
     $("#poetic-computation").toggleClass("hide");
     $("#link-poetic-computation").toggleClass("active-link");
-    $("#poetic-computation").removeClass("stack");
+    $("#poetic-computation").removeClass("stack").removeClass("hidepages");
   });
 
   $("#link-zoom").click(function(){
     $("#zoom").toggleClass("hide");
     $("#link-zoom").toggleClass("active-link");
-    $("#zoom").removeClass("stack");
+    $("#zoom").removeClass("stack").removeClass("hidepages");
   });
 
   $("#link-dark-matter").click(function(){
     $("#dark-matter").toggleClass("hide");
     $("#link-dark-matter").toggleClass("active-link");
-    $("#dark-matter").removeClass("stack");
+    $("#dark-matter").removeClass("stack").removeClass("hidepages");
   });
 
   $(function() {
