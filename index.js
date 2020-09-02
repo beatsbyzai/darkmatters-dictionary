607ee5c45a3515e0362b0cdf545c1b8ee34caa03
Zainab Aliyu <zoaliyu@gmail.com>
2020-09-02 01:44:34 -0400

    update type, add all key pages

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
+
 $( document ).ready(function() {
+
+
 
   var pages = ["poetic-computation", "zoom", "dark-matter"];
 
@@ -16,18 +20,41 @@ $( document ).ready(function() {
 
   $("#link-hide").click(function(){
     $(".page").toggleClass("hidepages");
+    // $(".back").toggleClass("z");
+    // $(".spine").toggleClass("z");
+    // $(".cover").toggleClass("z");
   });
 
   $("#intro-link").click(function(){
     $("#introduction").removeClass("introduction-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
     $("#contributors").addClass("contributors-z").removeClass("current-z");
     $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
+    $("#how-to-cite").addClass("cite-z").removeClass("current-z");
+    $("#public-readings").addClass("read-z").removeClass("current-z");
+    $("#link-hide").removeClass("hide");
+    $("#link-stack").removeClass("hide");
+
+    $("#intro-link").addClass("strike");
+    $("#contrib-link").removeClass("strike");
+    $("#ack-link").removeClass("strike");
+    $("#cite-link").removeClass("strike");
+    $("#read-link").removeClass("strike");
   });
 
   $("#contrib-link").click(function(){
     $("#contributors").removeClass("contributors-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
     $("#introduction").addClass("introduction-z").removeClass("current-z");
     $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
+    $("#public-readings").addClass("read-z").removeClass("current-z");
+    $("#how-to-cite").addClass("cite-z").removeClass("current-z");
+    $("#link-hide").removeClass("hide");
+    $("#link-stack").removeClass("hide");
+
+    $("#intro-link").removeClass("strike");
+    $("#contrib-link").addClass("strike");
+    $("#ack-link").removeClass("strike");
+    $("#cite-link").removeClass("strike");
+    $("#read-link").removeClass("strike");
   });
 
 
@@ -35,7 +62,50 @@ $( document ).ready(function() {
     $("#acknowledgements").removeClass("acknowledgements-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
     $("#contributors").addClass("contributors-z").removeClass("current-z");
     $("#introduction").addClass("introduction-z").removeClass("current-z");
+    $("#public-readings").addClass("read-z").removeClass("current-z");
+    $("#how-to-cite").addClass("cite-z").removeClass("current-z");
+    $("#link-hide").removeClass("hide");
+    $("#link-stack").removeClass("hide");
+
+    $("#intro-link").removeClass("strike");
+    $("#contrib-link").removeClass("strike");
+    $("#ack-link").addClass("strike");
+    $("#cite-link").removeClass("strike");
+    $("#read-link").removeClass("strike");
   });
+
+  $("#cite-link").click(function(){
+    $("#how-to-cite").removeClass("cite-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
+    $("#introduction").addClass("introduction-z").removeClass("current-z");
+    $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
+    $("#contributors").addClass("contributors-z").removeClass("current-z");
+    $("#public-readings").addClass("read-z").removeClass("current-z");
+    $("#link-hide").removeClass("hide");
+    $("#link-stack").removeClass("hide");
+
+    $("#intro-link").removeClass("strike");
+    $("#contrib-link").removeClass("strike");
+    $("#ack-link").removeClass("strike");
+    $("#cite-link").addClass("strike");
+    $("#read-link").removeClass("strike");
+  });
+
+  $("#read-link").click(function(){
+    $("#public-readings").removeClass("read-z").addClass("current-z").removeClass("hidepages").removeClass("hide");
+    $("#introduction").addClass("introduction-z").removeClass("current-z");
+    $("#acknowledgements").addClass("acknowledgements-z").removeClass("current-z");
+    $("#contributors").addClass("contributors-z").removeClass("current-z");
+    $("#how-to-cite").addClass("cite-z").removeClass("current-z");
+    $("#link-hide").removeClass("hide");
+    $("#link-stack").removeClass("hide");
+
+    $("#intro-link").removeClass("strike");
+    $("#contrib-link").removeClass("strike");
+    $("#ack-link").removeClass("strike");
+    $("#cite-link").removeClass("strike");
+    $("#read-link").addClass("strike");
+  });
+
 
   $("#link-poetic-computation").click(function(){
     $("#poetic-computation").toggleClass("hide");
@@ -58,5 +128,15 @@ $( document ).ready(function() {
   $(function() {
       $( ".page" ).draggable();
     });
+
+
+    // if ($(".page").visible()) {
+    //   $("#link-hide").removeClass("hide");
+    //   $("#link-stack").removeClass("hide");
+    // } else {
+    //   $("#link-hide").addClass("hide");
+    //   $("#link-stack").addClass("hide");
+    // }
+
 
 });
